2ad9865bafa6b2dacb21960ca81334b0f94d491c
xinhq <1983318935@qq.com>
2026-06-08 20:23:38 +0800

    feat: 检出代码

--- openai-code-review-sdk/src/main/java/cn/xiaoxin/sdk/OpenAiCodeReview.java
+++ openai-code-review-sdk/src/main/java/cn/xiaoxin/sdk/OpenAiCodeReview.java
@@ -1,5 +1,10 @@
 package cn.xiaoxin.sdk;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * @Author: xhq
  * @CreateTime: 2026-06-02
@@ -7,7 +12,27 @@ package cn.xiaoxin.sdk;
  * @Version: 1.0
  */
 public class OpenAiCodeReview {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         System.out.println("测试执行");
+
+
+        // 1. 代码检出
+        ProcessBuilder processBuilder = new ProcessBuilder("git", "diff", "HEAD~1", "HEAD");
+        processBuilder.directory(new File("."));
+
+        Process process = processBuilder.start();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line;
+
+        StringBuilder diffCode = new StringBuilder();
+        while ((line = reader.readLine()) != null) {
+            diffCode.append(line);
+        }
+
+        int exitCode = process.waitFor();
+        System.out.println("Exited with code:" + exitCode);
+
+        System.out.println("评审代码：" + diffCode.toString());
     }
 }
