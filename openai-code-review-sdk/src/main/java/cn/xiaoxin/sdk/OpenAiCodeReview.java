f15830158e1e29d86d50c17c9e1cc6bed900aa40
xinhq <1983318935@qq.com>
2026-06-09 20:47:14 +0800

    feat: 代码评审 V4

--- openai-code-review-sdk/src/main/java/cn/xiaoxin/sdk/OpenAiCodeReview.java
+++ openai-code-review-sdk/src/main/java/cn/xiaoxin/sdk/OpenAiCodeReview.java
@@ -1,6 +1,8 @@
 package cn.xiaoxin.sdk;
 
+import cn.xiaoxin.sdk.domain.model.ChatCompletionRequest;
 import cn.xiaoxin.sdk.domain.model.ChatCompletionSyncResponse;
+import cn.xiaoxin.sdk.domain.model.Model;
 import cn.xiaoxin.sdk.types.util.BearerTokenUtils;
 import com.alibaba.fastjson2.JSON;
 
@@ -8,6 +10,7 @@ import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 
 /**
  * @Author: xhq
@@ -69,9 +72,19 @@ public class OpenAiCodeReview {
                 + "]"
                 + "}";
 
+        ChatCompletionRequest chatCompletionRequest = new ChatCompletionRequest();
+        chatCompletionRequest.setModel(Model.GLM_4_FLASH.getCode());
+        chatCompletionRequest.setMessages(new ArrayList<ChatCompletionRequest.Prompt>() {
+            private static final long serialVersionUID = -7988151926241837899L;
 
-        try(OutputStream os = connection.getOutputStream()){
-            byte[] input = jsonInpuString.getBytes(StandardCharsets.UTF_8);
+            {
+                add(new ChatCompletionRequest.Prompt("user", "你是一个高级编程架构师，精通各类场景方案、架构设计和编程语言请，请您根据git diff记录，对代码做出评审。代码如下:"));
+                add(new ChatCompletionRequest.Prompt("user", diffCode));
+            }
+        });
+
+        try (OutputStream os = connection.getOutputStream()) {
+            byte[] input = JSON.toJSONString(chatCompletionRequest).getBytes(StandardCharsets.UTF_8);
             os.write(input);
         }
 
@@ -82,12 +95,14 @@ public class OpenAiCodeReview {
         String inputLine;
 
         StringBuilder content = new StringBuilder();
-        while ((inputLine = in.readLine()) != null){
+        while ((inputLine = in.readLine()) != null) {
             content.append(inputLine);
         }
 
         in.close();
         connection.disconnect();
+
+        System.out.println("评审结果：" + content.toString());
 
         ChatCompletionSyncResponse response = JSON.parseObject(content.toString(), ChatCompletionSyncResponse.class);
         return response.getChoices().get(0).getMessage().getContent();
